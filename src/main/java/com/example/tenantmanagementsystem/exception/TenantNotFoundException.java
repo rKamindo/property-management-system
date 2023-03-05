00f87e5e5ee0e1884f3cab59f9f39104deb72e9d
rKAmindo <randykamindo@gmail.com>
2023-03-04 22:16:59 -0500

    Add ResourceNotFoundException, replace TenantNotFoundException

--- src/main/java/com/example/tenantmanagementsystem/exception/TenantNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.tenantmanagementsystem.exception;
-
-
-public class TenantNotFoundException extends RuntimeException {
-    public TenantNotFoundException(String message) {
-        super(message);
-    }
-}
