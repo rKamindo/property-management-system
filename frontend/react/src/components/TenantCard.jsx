b7f43cf689348cb1993e6fb975c3ea5f36cf5daf
rKAmindo <randykamindo@gmail.com>
2023-03-18 13:56:06 -0400

    Tweak Tenant Card to always have a badge, resident or non-resident

--- frontend/react/src/components/TenantCard.jsx
+++ frontend/react/src/components/TenantCard.jsx
@@ -38,7 +38,7 @@ export default function CardWithImage({id, name, email, phone, gender, apartment
                 overflow={'hidden'}
             >
                 <Image
-                    h={'80px'}
+                    h={'120px'}
                     w={'full'}
                     src={
                     'https://images.unsplash.com/photo-1612865547334-09cb8cb455da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
@@ -53,9 +53,9 @@ export default function CardWithImage({id, name, email, phone, gender, apartment
                        </Heading>
                         <Text color={'gray.500'}>{email}</Text>
                         <Text color={'gray.500'}>{phone} | {gender}</Text>
-                        {isResident && <HStack>
-                             <Badge colorScheme="green">Resident</Badge>
-                            <Text color={'gray.500'}>Unit {apartmentNumber}</Text>
+                        <HStack>
+                            <Badge colorScheme={isResident ? "green" : "red"}>{isResident ? "Resident" : "Non-Resident"}    </Badge>
+                            {isResident && <Text color={'gray.500'}>Unit {apartmentNumber}</Text>}
                         </HStack>}
                     </Stack>
                     <Stack direction={'row'} justify={'center'}>
